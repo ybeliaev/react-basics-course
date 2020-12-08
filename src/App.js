7b45acd29857d882e17114107fe9460c76ee27cf
Jorgen <ybeliaev@gmail.com>
2020-12-08 13:42:12 +0200

    it's work, mom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React from "react";
 
+import ReactLogo from  "./lessons/1-jsx-to-hyperscript/close.svg"
 
-const title = React.createElement('h1', {class: "xxx"}, 'Примеры работ курса.');
-const subTitle = React.createElement('h2', {class: "xxx"}, 'Откоментируйте нужную строку с элементом.');
-function App() {   
+const title = React.createElement(
+  "h1",
+  { class: "xxx" },
+  "Примеры работ курса."
+);
+const subTitle = React.createElement(
+  "h2",
+  { class: "xxx" },
+  "Откоментируйте нужную строку с элементом."
+);
+function App() {
   return (
     <div className="wrapper">
-      <div>{title}{subTitle}</div>
+      <div>
+        {title}
+        {subTitle}
+        <img src={ReactLogo} alt="React Logo" />
+      </div>
     </div>
   );
 }
