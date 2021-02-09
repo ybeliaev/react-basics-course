6332e061ddb2fbac9b8bb4ecc1f72331018014f5
Jorgen <ybeliaev@gmail.com>
2021-02-09 14:35:54 +0200

    add onChange function as universal

--- src/lessons/13-profile-form/index.js
+++ src/lessons/13-profile-form/index.js
@@ -21,36 +21,47 @@ export default function Lesson13() {
   });
   let [errors, setErrors] = useState({});
 
-  function onChange() {}
+  function onChange(event) {
+    let {
+      target: { value, name, type, checked },
+    } = event;
+    value = type == "checkbox" ? checked : value;
+    setInputs((inputs) => ({ ...inputs, [name]: value })); // [name] - атрибут name у input
+  }
+  function onSubmit(event) {
+    event.preventDefault();
+  }
   return (
     <div className="wrapper_lesson">
       <h3>Урок 13</h3>
       <h4>Profile Form</h4>
-      <form className="w-50 fs-5">
+      <form className="w-50 fs-5" onSubmit={onSubmit}>
         <div className="mb-3">
-          <label htmlFor="exampleInputPassword1" className="form-label">
+          <label htmlFor="password-13" className="form-label">
             Username
           </label>
           <span>&nbsp;(*)</span>
           <input
             type="text"
+            name="userName"
             className="form-control"
-            id="exampleInputPassword1"
+            id="password-13"
             value={inputs.userName}
             onChange={onChange}
           />
         </div>
         <div className="mb-3">
-          <label htmlFor="exampleInputEmail1" className="form-label">
+          <label htmlFor="email-13" className="form-label">
             Email
           </label>
           <span>&nbsp;(*)</span>
           <input
             type="email"
             className="form-control"
-            id="exampleInputEmail1"
+            id="email-13"
             aria-describedby="emailHelp"
             value={inputs.email}
+            onChange={onChange}
           />
         </div>
 
@@ -59,12 +70,13 @@ export default function Lesson13() {
           <span>&nbsp;(*)</span>
           <br />
           <textarea
-            name="body"
+            name="about"
             rows={5}
             className="form-control"
             value={null}
             onChange={null}
             value={inputs.about}
+            onChange={onChange}
           />
         </div>
         <button type="submit" className="btn btn-primary">
