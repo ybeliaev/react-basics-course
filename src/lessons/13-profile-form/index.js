a634bcdcd76d8b458ab00872f2e6d8d943e61278
Jorgen <ybeliaev@gmail.com>
2021-02-10 15:22:09 +0200

    func convert intagrate to code to onSubmit()

--- src/lessons/13-profile-form/index.js
+++ src/lessons/13-profile-form/index.js
@@ -10,53 +10,58 @@ let schema = yup.object().shape({
   email: yup.string().email().required(),
   about: yup.string().min(3).max(500),
 });
-schema
-  .validate(
-    { userName: "a", email: "a sdasdas@asd.com" },
-    { abortEarly: false }
-  )
-  .then((x) => console.log(x))
-  .catch((e) => console.dir(e.inner));
-// e:
-// Object:
-// errors: (2) ["userName must be at least 3 characters", "email must be a valid email"]
-// inner: (2) [ValidationError: userName must be at least 3 characters at createError (http://localhost:3000/0…, ValidationError: email must be a valid email at createError (http://localhost:3000/0.a0741293c4…]
-// message: "2 errors occurred"
-// name: "ValidationError"
-// path: undefined
-// type: undefined
-// value: {userName: "a", email: "a sdasdas@asd.com"}
+// schema
+//   .validate({ userName: "yura", email: "aasd@asd.com" }, { abortEarly: false })
+//   .then((x) => console.log(x))
+//   .catch((e) => console.dir(e)); // сюда зайдёт если есть ошибка и соответственно появится объект e
 
-// e.inner:
-//Array(2)
-// 0: ValidationError: userName must be at least 3 characters at createError (http://localhost:3000/0.a0741293c468a136e8c2.hot-update.js:2786:21) at http://localhost:3000/0.a0741293c468a136e8c2.hot-update.js:2804:139
-//   errors: ["userName must be at least 3 characters"]
-//   inner: []
-//   message: "userName must be at least 3 characters"
-//   name: "ValidationError"
-//   params: {value: "a", originalValue: "a", label: undefined, path: "userName", min: 3}
-//   path: "userName"
-//   type: "min"
-//   value: "a"
-//   stack: "ValidationError: userName must be at least 3 characters↵
-// 1: ValidationError: email must be a valid email at createError (http://localhost:3000/0.a0741293c468a136e8c2.hot-update.js:2786:21) at http://localhost:3000/0.a0741293c468a136e8c2.hot-update.js:2804:139
-//   errors: ["email must be a valid email"]
-//   inner: []
-//   message: "email must be a valid email"
-//   name: "ValidationError"
-//   params: {value: "a sdasdas@asd.com", originalValue: "a sdasdas@asd.com", label: undefined, path: "email", regex: /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A…]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i}
-//   path: "email"
-//   type: "email"
-//   value: "a sdasdas@asd.com"
-//   stack: "ValidationError: email must be a valid email
-// функция для конвертации ошибок в нужный формат: оставляю для каждого поля только первую ошибку
+// const e = {
+//   errors: ["userName must be at least 3 characters", "email must be a valid email"],
+//   inner: [
+//     {
+//       errors: ["userName must be at least 3 characters"],
+//       inner: [],
+//       message: "userName must be at least 3 characters",
+//       name: "ValidationError",
+//       params: {value: "a", originalValue: "a", label: undefined, path: "userName", min: 3},
+//       path: "userName",
+//       type: "min",
+//       value: "a",
+//       stack: "ValidationError: userName must be at least 3 characters"
+//     },
+//     {
+//       errors: ["email must be a valid email"],
+//       inner: [],
+//       message: "email must be a valid email",
+//       name: "ValidationError",
+//       params: {value: "a sdasdas@asd.com", originalValue: "a sdasdas@asd.com", label: undefined, path: "email", regex: /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A…]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i},
+//       path: "email",
+//       type: "email",
+//       value: "a sdasdas@asd.com",
+//       stack: "ValidationError: email must be a valid email"
+//     },
+//     message: "2 errors occurred",
+//     name: "ValidationError",
+//     path: undefined,
+//     type: undefined,
+//     value:{
+//      email: "a sdasdas@asd.com"
+//      userName: "a"
+//     }
+//   ]
+// }
+// функция для конвертации ошибок в объект с ключами-именами инпутов: оставляю для каждого поля только первую ошибку
 function convert(errors) {
-  return errors.inner.reduce((acc, item) => {
-    let name = (item.path || "").includes(".")
-      ? item.path.split(".")[0]
-      : item.path || "";
-    return acc[item.path || ""] ? acc : { ...acc, [name]: item.message };
-  }, {});
+  if (errors.inner) {
+    return errors.inner.reduce((acc, item) => {
+      let name = (item.path || "").includes(".")
+        ? item.path.split(".")[0]
+        : item.path || "";
+      return acc[item.path || ""] ? acc : { ...acc, [name]: item.message };
+    }, {});
+  } else {
+    throw errors;
+  }
 }
 
 export default function Lesson13() {
@@ -82,11 +87,15 @@ export default function Lesson13() {
   }
   async function onSubmit(event) {
     event.preventDefault();
-    let error = await schema
+    let errors = await schema
       .validate(inputs, { abortEarly: false })
       .then((_) => ({}))
       .catch(convert);
-    console.log(inputs);
+
+    setErrors(errors);
+    if (!Object.keys(errors).length) {
+      console.log(inputs);
+    }
   }
   return (
     <div className="wrapper_lesson">
@@ -97,7 +106,7 @@ export default function Lesson13() {
           <label htmlFor="password-13" className="form-label">
             Username
           </label>
-          <span>&nbsp;({errors.username || "*"})</span>
+          <span>&nbsp;({errors.userName || "*"})</span>
           <input
             type="text"
             name="userName"
