54086c7760416b445d716675eb9c81231b70d5a9
Jorgen <ybeliaev@gmail.com>
2021-02-22 15:23:10 +0200

    three variants fetch

--- src/lessons/15-git_api/index.js
+++ src/lessons/15-git_api/index.js
@@ -1,5 +1,56 @@
 import React from "react";
 
+function createMarkup1() {
+  return {
+    __html: `<pre>
+        <code>
+    (async function () {
+        let res = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+        let data = await res.json();
+        console.log(data);
+        console.log(res);
+        })();
+    </code>
+    </pre>
+      `,
+  };
+}
+function createMarkup2() {
+  return {
+    __html: `<pre>
+          <code>
+    function() {
+        fetch("https://jsonplaceholder.typicode.com/todos/1")
+        .then(resp => [resp, resp.json()])
+        .then(([resp,data]) => {console.log(data); console.log(res);})
+    };
+    
+    // или
+
+    function() {
+        fetch("https://jsonplaceholder.typicode.com/todos/1")
+        .then(resp => resp.json().then(data => {console.log(data); console.log(res)}
+    };
+      </code>
+      </pre>
+    `,
+  };
+}
+
+function MyComponent1() {
+  return <div dangerouslySetInnerHTML={createMarkup1()} />;
+}
+function MyComponent2() {
+  return <div dangerouslySetInnerHTML={createMarkup2()} />;
+}
+// let data = null;
+(async function () {
+  let res = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+  let data = await res.json();
+  console.log(data);
+  console.log(res);
+})();
+
 export default function Lesson15() {
   return (
     <div className="wrapper_lesson">
@@ -8,6 +59,10 @@ export default function Lesson15() {
       <p>
         fetch install like as <strong>cross-fetch</strong>
       </p>
+      <MyComponent1 />
+      <p>Такая запись даёт возможность работать с несколькими переменными</p>
+      <p>Похожее в формате then:</p>
+      <MyComponent2 />
     </div>
   );
 }
