2c0bbf62e9e5af7426581566e48916d9157e8fec
Jorgen <ybeliaev@gmail.com>
2021-03-01 15:43:04 +0200

    output repositores is adding

--- src/lessons/16-git_api/index.js
+++ src/lessons/16-git_api/index.js
@@ -4,10 +4,11 @@ import { makeApiClient } from "./api";
 
 let client = makeApiClient("https://api.github.com");
 
-export default function Lesson16() {
+export default function Lesson15() {
   let [user, setUser] = useState(null);
-  let [loading, setLoading] = useState(true);
+  let [repos, setRepos] = useState(null);
   let [error, setError] = useState(null);
+  let [loading, setLoading] = useState(true);
 
   useEffect(() => {
     client
@@ -19,6 +20,15 @@ export default function Lesson16() {
       .catch((error) => setError(error)); // or catch(setError)
   }, []);
 
+  useEffect(() => {
+    client
+      .fetchJSON("/users/ybeliaev/repos?sort=created&per_page=5")
+      .then((repos) => {
+        setRepos(repos);
+        setLoading(false);
+      })
+      .catch((error) => setError(error)); // or catch(setError)
+  }, []);
   if (error) {
     return <Error error={error} />;
   }
@@ -29,14 +39,33 @@ export default function Lesson16() {
 
   return (
     <div className="wrapper_lesson fs-4">
-      <h3>Урок 16</h3>
-      <h4>Git API. Multiple requests</h4>
+      <h3>Урок 16.</h3>
+      <h4>Git API. Multiple requests.</h4>
+      <h5>My name is {user.name}</h5>
+      <img
+        className="border border-primary"
+        width="150px"
+        src={user.avatar_url}
+      />
+      <span className="ms-2 badge bg-primary">{user.login}</span>
+      <h4 className="h4">Repositories</h4>
+      {repos.map((repo) => {
+        return (
+          <ul className="list-group" key={repo.id}>
+            <li className="list-group-item">
+              <h4 className="h5">
+                <a href={repo.html_url}>{repo.name}</a> ★{" "}
+                {repo.stargazers_count}
+              </h4>
+            </li>
+          </ul>
+        );
+      })}
     </div>
   );
 }
 
 function Error({ error }) {
-  // объект error приводить к строке - временное решение
   return (
     <h1>
       <span className="badge bg-danger">{String(error)}</span>
