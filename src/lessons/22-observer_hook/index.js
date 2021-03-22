90113f004edf4c6335c45d73adccc7266fac893e
Jorgen <ybeliaev@gmail.com>
2021-03-22 17:12:50 +0200

    realize filter for list || lesson22

--- src/lessons/22-observer_hook/index.js
+++ src/lessons/22-observer_hook/index.js
@@ -5,20 +5,39 @@ export default function Lesson22() {
   const [page, setPage] = useState(1);
   let limit = 10;
   const { todos, loading } = useSearch(limit, page);
+  const [querySearch, setQuerySearch] = useState("");
   const parentRef = useRef();
   const childRef = useRef();
 
-  const elements = todos.map((todo) => (
-    <li key={todo.id} className="list-group-item list-group-item-action">
-      {todo.id}. {todo.title}
-      <div ref={childRef}></div>
-    </li>
-  ));
+  function handleSearch(value) {
+    console.log(value);
+    console.log(todos);
+  }
+
+  const elements = todos
+    .filter((val) => {
+      if (querySearch == "") {
+        return val;
+      } else if (val.title.toLowerCase().includes(querySearch.toLowerCase())) {
+        return val;
+      }
+    })
+    .map((todo) => (
+      <li key={todo.id} className="list-group-item list-group-item-action">
+        {todo.id}. {todo.title}
+        <div ref={childRef}></div>
+      </li>
+    ));
   return (
     <div className="wrapper_lesson fs-4">
       <h3>Урок 22.</h3>
       <h5 className="text-center">Hook pagination</h5>
-      <input type="text" className="form-control mb-2 w-50"></input>
+      <input
+        type="text"
+        className="form-control mb-2 w-50"
+        onChange={(e) => setQuerySearch(e.target.value)}
+        placeholder="search.."
+      ></input>
       <ul
         className="list-group w-50 overflow-auto"
         style={{ maxHeight: "500px" }}
