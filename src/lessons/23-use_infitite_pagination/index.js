2a95f9230427e5b308228f200fb6650717dd7fa1
Jorgen <ybeliaev@gmail.com>
2021-04-05 17:26:07 +0300

    hoooh...less23 work change position parentRef to <ul>

--- src/lessons/23-use_infitite_pagination/index.js
+++ src/lessons/23-use_infitite_pagination/index.js
@@ -1,30 +1,44 @@
 import React, { useEffect, useState, useRef, useCallback } from "react";
+import useScroll from "./useScroll";
 
 export default function Lesson23() {
   const [todos, setTodos] = useState([]);
   const [page, setPage] = useState(1);
   const limit = 10;
 
-  const fetchTodos = (limit, page) => {
+  const parentRef = useRef();
+  const childRef = useRef();
+
+  const intersected = useScroll(parentRef, childRef, () =>
+    fetchTodos(limit, page)
+  );
+
+  function fetchTodos(limit, page) {
     fetch(
       `https://jsonplaceholder.typicode.com/todos?_limit=${limit}&_page=${page}`
     )
       .then((response) => response.json())
-      .then((json) => setTodos(json));
-  };
-  useEffect(() => {
-    fetchTodos(limit, page);
-  }, []);
+      .then((json) => {
+        setTodos((prev) => [...prev, ...json]);
+        setPage((prev) => prev + 1);
+        console.log("do it");
+      })
+      .catch((error) => console.log(error));
+  }
+  //   useEffect(() => {
+  //     fetchTodos(limit, page);
+  //   }, []);
 
   return (
     <div className="wrapper_lesson fs-4">
       <h3>Урок 23.</h3>
       <h5 className="text-center">Hook infinite pagination.</h5>
       <ul
+        ref={parentRef}
         className="list-group w-50 overflow-auto"
         style={{ maxHeight: "500px" }}
       >
-        {todos.map((todo) => {
+        {todos.map((todo, idx) => {
           return (
             <li
               key={todo.id}
@@ -34,6 +48,10 @@ export default function Lesson23() {
             </li>
           );
         })}
+        <div
+          ref={childRef}
+          style={{ height: 20, backgroundColor: "tomato", marginBottom: 10 }}
+        ></div>
       </ul>
     </div>
   );
---
+++ src/lessons/23-use_infitite_pagination/useScroll.js
@@ -0,0 +1,22 @@
+import { useRef, useEffect } from "react";
+
+export default function useScroll(parentRef, childRef, callback) {
+  const observer = useRef();
+
+  useEffect(() => {
+    const options = {
+      root: parentRef.current,
+      rootMargin: "0px",
+      threshold: 0,
+    };
+    observer.current = new IntersectionObserver(([target]) => {
+      if (target.isIntersecting) {
+        callback();
+      }
+    }, options);
+    observer.current.observe(childRef.current);
+    return function () {
+      observer.current.unobserve(childRef.current);
+    };
+  }, [callback]);
+}
